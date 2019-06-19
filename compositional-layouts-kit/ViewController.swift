544f6ca03424957c89d4f88edb588adce4a4b30f
Astemir Eleev <astemireleev@gmail.com>
2019-06-19 14:53:25 +0300

    Initial Commit

---
+++ compositional-layouts-kit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  compositional-layouts-kit
+//
+//  Created by Astemir Eleev on 19/06/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
