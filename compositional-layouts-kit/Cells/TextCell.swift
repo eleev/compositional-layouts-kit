e855c6c89de59482d090287204cc00735df19b9d
Astemir Eleev <astemireleev@gmail.com>
2019-06-21 19:44:20 +0300

    Created UICollectionView cells

---
+++ compositional-layouts-kit/Cells/TextCell.swift
@@ -0,0 +1,46 @@
+//
+//  TextCell.swift
+//  compositional-layouts-kit
+//
+//  Created by Astemir Eleev on 19/06/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class TextCell: UICollectionViewCell, ReuseIdentifiable {
+    
+    // MARK: - Properties
+    
+    let label = UILabel()
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemnted")
+    }
+    
+}
+
+// MARK: - Conformance to `Configurable` protocol
+extension TextCell: Configurable {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        let inset = CGFloat(10)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+            ])
+    }
+}
