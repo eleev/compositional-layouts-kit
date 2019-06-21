8c2e1953ea2fa145bd0492ae94168756eb51509d
Astemir Eleev <astemireleev@gmail.com>
2019-06-21 19:47:07 +0300

    Created UIColor+Colors extension

---
+++ compositional-layouts-kit/Extensions/UIColor+Colors.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Colors.swift
+//  compositional-layouts-kit
+//
+//  Created by Astemir Eleev on 21/06/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import class UIKit.UIColor
+
+extension UIColor {
+    static var cornflowerBlue: UIColor {
+        return UIColor(displayP3Red: 100.0 / 255.0, green: 149.0 / 255.0, blue: 237.0 / 255.0, alpha: 1.0)
+    }
+}
