5c8ca7356c5e3962f3ccd8a2b1848bab48b789f4
Astemir Eleev <astemireleev@gmail.com>
2019-06-22 09:44:44 +0300

    Created GalleryViewController class

---
+++ compositional-layouts-kit/Compositional Layouts VCs/GalleryViewController.swift
@@ -0,0 +1,85 @@
+//
+//  GalleryViewController.swift
+//  compositional-layouts-kit
+//
+//  Created by Astemir Eleev on 19/06/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Gallery Layout"
+        configureHierarchy()
+        configureDataSource()
+    }
+}
+
+extension GalleryViewController {
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .fractionalHeight(1.0))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.scrollDirection = .horizontal
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        layout.configuration = config
+        
+        return layout
+    }
+}
+
+extension GalleryViewController {
+    private func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        collectionView.isDirectionalLockEnabled = true
+        collectionView.isPagingEnabled = true
+        collectionView.alwaysBounceVertical = false
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.register(ImageCell.self, forCellWithReuseIdentifier: ImageCell.reuseIdentifier)
+        view.addSubview(collectionView)
+    }
+    private func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            
+            // Get a cell of the desired kind.
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: ImageCell.reuseIdentifier,
+                for: indexPath) as? ImageCell else { fatalError("Could not create new cell") }
+            cell.image = ImageFactory.produce(using: .random)
+            
+            return cell
+        }
+        
+        // initial data
+        let snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<10))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
