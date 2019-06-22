3e1899789694aff29ded42e6fd5c8e444bc7db58
Astemir Eleev <astemireleev@gmail.com>
2019-06-22 09:46:16 +0300

    Created BadgeSupplementaryView class

---
+++ compositional-layouts-kit/Supplimentary Views/BadgeSupplementaryView.swift
@@ -0,0 +1,62 @@
+//
+//  BadgeSupplementaryView.swift
+//  compositional-layouts-kit
+//
+//  Created by Astemir Eleev on 19/06/2019.
+//  Copyright © 2019 Astemir Eleev. All rights reserved.
+//
+
+import UIKit
+
+class BadgeSupplementaryView: UICollectionReusableView, ReuseIdentifiable {
+    
+    // MARK: - Properties
+    
+    let label = UILabel()
+    
+    override var frame: CGRect {
+        didSet {
+            configureBorder()
+        }
+    }
+    override var bounds: CGRect {
+        didSet {
+            configureBorder()
+        }
+    }
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Not implemented")
+    }
+}
+
+// MARK: - Conformance to `Configurable` protocol
+extension BadgeSupplementaryView: Configurable {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.textAlignment = .center
+        label.textColor = .black
+        backgroundColor = .green
+        configureBorder()
+    }
+    func configureBorder() {
+        let radius = bounds.width / 2.0
+        layer.cornerRadius = radius
+        layer.borderColor = UIColor.black.cgColor
+        layer.borderWidth = 1.0
+    }
+}
